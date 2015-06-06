c520f29d4b5fdfcbd868e80a817a2c3f6c5caaa5
罗雅文 <bellwind@foxmail.com>
2015-06-06 21:00:10 +0800

    coding pratice

---
+++ 026_Remove_Duplicates_from_Sorted_Array.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        if(nums.size() == 0)
+            return 0;
+        int cnt = 0;
+        for(int i = 1; i < nums.size(); i++) if(nums[cnt] < nums[i]) {
+            nums[++cnt] = nums[i];
+        }
+        return cnt + 1;
+    }
+};
