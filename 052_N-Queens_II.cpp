50c7973d79223676a28b5139cc72a43f3f35d539
罗雅文 <bellwind@foxmail.com>
2015-06-07 00:17:42 +0800

    coding pratice

---
+++ 052_N-Queens_II.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int totalNQueens(int n) {
+        ans.clear();
+        rv = vector<bool>(n, false);
+        cv = vector<bool>(n, false);
+        nd = vector<bool>(n * 2 - 1, false);
+        bd = vector<bool>(n * 2 - 1, false);
+        vector<string> tmp(n, string(n, '.'));
+        nn = n;
+        dfs(tmp, 0);
+        return ans.size();
+    }
+private:
+    vector<vector<string>> ans;
+    vector<bool> rv, cv, nd, bd;
+    int nn;
+    bool Set(vector<string>& x, int r, int c) {
+        if(rv[r] || cv[c] || bd[r + c] || nd[r - c + nn - 1])
+            return false;
+        rv[r] = cv[c] = bd[r + c] = nd[r - c + nn - 1] = true;
+        x[r][c] = 'Q';
+        return true;
+    }
+    void unSet(vector<string>& x, int r, int c) {
+        x[r][c] = '.';
+        rv[r] = cv[c] = bd[r + c] = nd[r - c + nn - 1] = false;
+    }
+    void dfs(vector<string>& cur, int k) {
+        if(k == nn) {
+            ans.push_back(cur);
+            return ;
+        }
+        for(int i = 0; i < nn; i++) {
+            if(Set(cur, k, i)) {
+                dfs(cur, k + 1);
+                unSet(cur, k, i);
+            }
+        }
+    }
+};
