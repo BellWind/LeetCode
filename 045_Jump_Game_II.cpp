96456971d69d91a11ce17d24f60eac3595ba74ad
罗雅文 <bellwind@foxmail.com>
2015-06-07 00:01:29 +0800

    coding pratice

---
+++ 045_Jump_Game_II.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int dn = 0, up = 0, ans = 0;
+        while(up < nums.size() - 1) {
+            ans++;
+            int tmp = up;
+            for(int i = dn; i <= up; i++)
+                tmp = max(tmp, i + nums[i]);
+            dn++, up = max(dn, tmp);
+        }
+        return ans;
+    }
+};
